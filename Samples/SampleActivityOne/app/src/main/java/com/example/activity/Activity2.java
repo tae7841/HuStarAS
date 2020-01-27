a6f3f481a1085f889273ebf35bda5f6e934b5e29
Youngsup Kim <idebtor@gmail.com>
2020-01-27 23:50:40 +0900

    up

---
+++ Samples/SampleActivityOne/app/src/main/java/com/example/activity/Activity2.java
@@ -0,0 +1,17 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+    }
+}
