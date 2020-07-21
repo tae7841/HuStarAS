b61c3cde3d53994f331f78f6920e877a1b0358cd
Youngsup Kim <idebtor@gmail.com>
2020-07-21 23:00:38 +0900

    up

---
+++ Joy/Hu024FrameLayout/app/src/main/java/org/joy/layout/MainActivity.java
@@ -0,0 +1,33 @@
+package org.joy.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    ImageView imageView2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("HuStar", "onCreate()");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        imageView2 = findViewById(R.id.imageView2);
+    }
+
+    public void onButtonClicked(View v) {
+        Log.d("HuStar", "onButtonClicked");
+        if (imageView.isShown()) {
+            imageView.setVisibility(View.INVISIBLE);
+            imageView2.setVisibility(View.VISIBLE);
+        } else {
+            imageView.setVisibility(View.VISIBLE);
+            imageView2.setVisibility(View.INVISIBLE);
+        }
+    }
+}
