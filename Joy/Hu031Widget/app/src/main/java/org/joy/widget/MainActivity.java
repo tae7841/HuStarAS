79b1fd588e876179ce27ddf18aaacef916020b91
Youngsup Kim <idebtor@gmail.com>
2020-07-22 13:15:02 +0900

    up

--- Joy/Hu031Widget/app/src/main/java/org/joy/widget/MainActivity.java
+++ Joy/Hu031Widget/app/src/main/java/org/joy/widget/MainActivity.java
@@ -2,6 +2,7 @@ package org.joy.widget;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,27 +16,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.button);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
-        textView.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                show_greeting(v);
-            }
-        });
         textView2 = findViewById(R.id.textView2);
         textView2.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                show_my_greeting(v);
+                // show_greeting(v);
+                Toast.makeText(getApplicationContext(), R.string.my_greeting, Toast.LENGTH_LONG).show();
+                Snackbar.make(v, R.string.my_greeting, Snackbar.LENGTH_LONG).show();
+                textView2.setText(R.string.my_greeting);
+                textView2.setTextColor(Color.BLUE);
             }
         });
     }
 
     public void show_greeting(View v) {
-        Toast.makeText(getApplicationContext(), R.string.greeting, Toast.LENGTH_LONG).show();
-    }
-    public void show_my_greeting(View v) {
+        Toast.makeText(getApplicationContext(), R.string.my_greeting, Toast.LENGTH_LONG).show();
         Snackbar.make(v, R.string.my_greeting, Snackbar.LENGTH_LONG).show();
+        textView2.setText(R.string.my_greeting);
+        textView2.setTextColor(Color.BLUE);
     }
 }
