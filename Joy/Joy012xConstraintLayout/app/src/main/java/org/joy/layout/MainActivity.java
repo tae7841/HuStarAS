5ff925cfb88ec11558fe0fa59e70c2c0734a6bfa
Youngsup Kim <idebtor@gmail.com>
2020-07-27 00:52:20 +0900

    up

---
+++ Joy/Joy012xConstraintLayout/app/src/main/java/org/joy/layout/MainActivity.java
@@ -0,0 +1,35 @@
+package org.joy.layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button 1 */
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    /** Called when the user taps the Send button 2 */
+    public void sendMessage2(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        String message = editText.getText().toString();
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
