4ab8ccd53fa5a0a3f95af35e07c521334e8dc2b1
Youngsup Kim <idebtor@gmail.com>
2021-07-19 08:48:17 +0900

    up

--- Joy/Joy0743RecyclerView/app/src/main/java/org/joy/view/OnPersonItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.joy.view;
-
-import android.view.View;
-
-public interface OnPersonItemClickListener {
-    public void onItemClick(PersonAdapter.ViewHolder holder, View view, int position);
-}
